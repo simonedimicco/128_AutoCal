a819a6ffc93eab99df170457024657f34de5346d
simone <simone.dimicco@uniroma1.it>
2026-04-13 16:32:28 +0200

    prova_pickle

---
+++ prova_pickle.py
@@ -0,0 +1,86 @@
+#%%
+from qlab.devices.tdc import QuTag
+import qlab.counting.counting as counting
+import qlab.counting.cocount as cocount
+from auto_classical import PowerSupplies
+from qlab.devices.KeithleyPowerSupply import KeithleyPowerSupply
+import logging
+from dmx_controller import DMXController
+#%%
+import numpy as np
+from numba import njit
+import time 
+import pyvisa as visa
+from tqdm import tqdm
+from WhiteLib import process_measurement
+from datetime import datetime
+strnow = lambda: datetime.now().strftime("%Y%m%d-%H%M%S")
+strtoday = lambda: datetime.now().strftime("%Y_%m_%d")
+strtimenow = lambda: datetime.now().strftime("%H:%M:%S")
+import time
+
+#%%
+def setloop(input):
+    loop =[0,0,0,5]
+    for channel in input:
+        if channel != 0 and channel != 5:
+            loop[channel-1]=channel 
+        else:
+            print('You are measuring the dark')
+
+    return loop
+
+'''
+INIZIALIZATION QU-TAGS
+'''
+#%%
+boxes_ind = QuTag.discover()
+
+#%%
+ind = [b'T 02 0010', b'T 02 0021']
+boxes = []
+for i in ind:
+    for j in boxes_ind:
+        if j._sn == i:
+           boxes.append(j)
+
+if len(ind) != len(boxes):
+    raise ValueError('scatole non trovate')
+
+#%%
+'''
+INIZIALIZATION QDMX
+'''
+#%%
+channel_a= 0
+channel_b= 1
+channel_c= 2
+channel_d= 3
+channel_e= 4
+channel_f= 5
+#%%
+dmx = DMXController(log_level=logging.DEBUG)
+#%%
+dmx.stop_looping()
+#%%
+print(dmx.get_data())
+#%%
+
+dmx.set_dwell_time(channel=channel_a, dwell_time=26)
+dmx.set_dwell_time(channel=channel_b, dwell_time=26)
+dmx.set_dwell_time(channel=channel_c, dwell_time=26)
+dmx.set_dwell_time(channel=channel_d, dwell_time=26)
+dmx.set_dwell_time(channel=channel_e, dwell_time=16)
+dmx.set_dwell_time(channel=channel_f, dwell_time=16)
+#%%
+loop = setloop((0,))
+dmx.set_active_outputs(loop)
+exposition = 0.1
+duration = 1
+repetitions = int(duration/exposition)
+measure = counting.get_raw_timestamps_multiple(boxes,exposition,num_acq=repetitions)
+tags = [(t,c) for t,c in measure]
+
+#%%
+import pickle
+pickle.dumps(tags)
